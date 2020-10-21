d971a871855a7348b844f0c3e61c042b210e46b6
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-20 20:26:43 -0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("nav").addEventListener("mouseover", audioHover);
+
+// function audioHover() {
+//   var aud = document.getElementById("navaudio");
+//     // aud.pause();
+//     // aud.currentTime = 0;
+//     aud.play();
+
+// }
