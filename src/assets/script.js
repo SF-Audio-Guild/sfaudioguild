6357e0ca62eb052c0e50963373d960da9563efe2
Julian <jumpingafterrain@gmail.com>
2023-07-10 15:38:21 +0200

    remove 11ty

--- src/assets/script.js
+++
@@ -1,8 +0,0 @@
-// Download Prompt
-function downloadPrompt(file) {
-  if(window.confirm('By downloading this package you agree to only use these files for personal use, not for resale or any other commercial purposes. Continue with download?')) {
-    var a = document.createElement('a')
-    a.href = file
-    a.click()
-  }
-}
